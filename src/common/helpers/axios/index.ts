0f059361b924c4bd6d1eed5c78a03a7f77005d2b
Hide on bush <hideonbushx123@gmail.com>
2022-04-07 14:19:44 -0300

    Updated URL

--- src/common/helpers/axios/index.ts
+++ src/common/helpers/axios/index.ts
@@ -1,4 +1,4 @@
-export const URL = 'http://localhost:3000/';
+export const URL = 'https://xiwvhk0ca8.execute-api.us-east-1.amazonaws.com/fin/';
 
 export const Headers = {
   'x-api-key': 'NN7ARbtuBy68bM78xQHFy3YDmLbUIPl676COm6Qa',
