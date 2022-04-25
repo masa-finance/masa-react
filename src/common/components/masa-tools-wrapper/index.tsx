cc7652e0a60a8e8e7cce4037feccb709e94229af
Hide on bush <hideonbushx123@gmail.com>
2022-04-25 09:05:17 -0300

    Provider

---
+++ src/common/components/masa-tools-wrapper/index.tsx
@@ -0,0 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+import React, { useCallback, useEffect, useState } from 'react';
+import { masaToolsProvider } from '../../helpers/provider';
+const XMasaToolsProvider = masaToolsProvider
+export interface MasaToolsWrapperProps {
+  children: React.ReactNode;
+}
+export function MasaToolsWrapper({ children }: MasaToolsWrapperProps) {
+  const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  const [token, setToken] = useState<undefined | string>(undefined);
+
+  const handleToken = useCallback(async () => {
+    if (isAuthenticated && !isLoading) {
+      const newToken = await getAccessTokenSilently();
+      setToken(newToken);
+    }
+  }, [isAuthenticated, isLoading, getAccessTokenSilently]);
+
+  useEffect(() => {
+    handleToken();
+  }, [handleToken]);
+
+
+  return <XMasaToolsProvider accessToken={token}>{children}</XMasaToolsProvider>;
+}
--- src/common/components/rest-tester/rest-tester.tsx
+++ src/common/components/rest-tester/rest-tester.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 import { RestMethod } from '../rest-method';
 import { masaRestClient } from '../../rest';
 import { AuthProvider } from '../auth-provider';
+import { MasaToolsWrapper } from '../masa-tools-wrapper';
 export function RestTester() {
   console.log(masaRestClient);
 
@@ -17,13 +18,15 @@ export function RestTester() {
   }, []);
   return (
     <AuthProvider>
-      <List
-        itemLayout="horizontal"
-        dataSource={methods}
-        renderItem={(item) => (
-          <RestMethod {...item.metadata} useMethod={item.useMethod} />
-        )}
-      ></List>
+      <MasaToolsWrapper>
+        <List
+          itemLayout="horizontal"
+          dataSource={methods}
+          renderItem={(item) => (
+            <RestMethod {...item.metadata} useMethod={item.useMethod} />
+          )}
+        ></List>
+      </MasaToolsWrapper>
     </AuthProvider>
   );
 }
