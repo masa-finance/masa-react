eb9c2ef086da5e533c7e2830e4ac86c1b7461d65
hide-on-bush-x <hideonbushx123@gmail.com>
2022-11-14 09:55:31 -0300

    Bundling fonts

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,7 @@
 const postcss = require('rollup-plugin-postcss');
 const replace = require('@rollup/plugin-replace');
 const sass = require('rollup-plugin-scss');
+const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config, options) {
@@ -11,12 +12,15 @@ module.exports = {
         watch: '',
         verbose: true,
         include: ['/**/*.css', '/**/*.scss', '/**/*.sass'],
-        output: 'dist/styles/style.css',
+        output: 'dist/style.css',
         failOnError: true,
       }),
       postcss({
         extensions: ['.css', '.scss', '.less'],
         use: ['sass', ['less', { javascriptEnabled: true }]],
+      }),
+      copy({
+        targets: [{ src: 'fonts/**/*', dest: 'dist/fonts' }],
       }),
     ]),
       (config.plugins = config.plugins.map((plugin) =>
