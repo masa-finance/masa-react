de9b2864a08bbdfaea5b23c42e72159a9219ca70
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-15 10:34:46 +0100

    make sure copy is only run once

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,6 +3,9 @@ const replace = require('@rollup/plugin-replace');
 const sass = require('rollup-plugin-scss');
 const copy = require('rollup-plugin-copy');
 
+// make sure copy is only added once
+let copyAdded = false;
+
 module.exports = {
   rollup(config, options) {
     config.plugins = [
@@ -19,15 +22,8 @@ module.exports = {
         extensions: ['.css', '.scss', '.less'],
         use: ['sass', ['less', { javascriptEnabled: true }]],
       }),
-      copy({
-        targets: [
-          {
-            src: 'fonts/**/*',
-            dest: 'dist/fonts',
-          },
-        ],
-      }),
     ];
+
     config.plugins = config.plugins.map((plugin) =>
       plugin.name === 'replace'
         ? replace({
@@ -36,6 +32,25 @@ module.exports = {
           })
         : plugin
     );
+
+    config.plugins = [
+      ...config.plugins,
+      !copyAdded
+        ? copy({
+            targets: [
+              {
+                src: 'fonts/**/*',
+                dest: 'dist/fonts',
+              },
+            ],
+            verbose: true,
+            copyOnce: true,
+          })
+        : false,
+    ];
+
+    copyAdded = true;
+
     return config;
   },
 };
