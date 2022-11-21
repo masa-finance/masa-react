66594a85c2a1ea1a1811457766409f8f91d37407
hide-on-bush-x <hideonbushx123@gmail.com>
2022-11-21 12:10:16 -0300

    removed unused variable

--- src/common/components/masa-interface/pages/connected/index.tsx
+++ src/common/components/masa-interface/pages/connected/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useMasa } from '../../../../helpers/provider/use-masa';
 import { MasaLoading } from '../../../masa-loading';
 
