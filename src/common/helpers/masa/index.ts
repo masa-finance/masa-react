d991b5616fd50d5c25ffa73b3713bc6b8ee0b72c
hide-on-bush-x <hideonbushx123@gmail.com>
2023-01-11 11:34:27 -0300

    Random Wallet generation

--- src/common/helpers/masa/index.ts
+++ src/common/helpers/masa/index.ts
@@ -1,4 +1,5 @@
 import { Masa } from '@masa-finance/masa-sdk';
+import { ethers, Wallet } from 'ethers';
 
 const config = {
   ...process.env,
@@ -10,13 +11,28 @@ const envs = {
   test: 'https://test.middleware.masa.finance/',
   local: 'http://localhost:4000/',
 };
+
+
+export const createRandomWallet = (): Wallet => {
+  console.info("Creating random wallet!");
+  const wallet = ethers.Wallet.createRandom();
+
+  wallet.connect(
+    new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/eth_goerli")
+  );
+  return wallet;
+};
+
+
 export const createNewMasa = (
   newWallet,
   env: 'dev' | 'beta' | 'test' | 'local' = 'dev'
 ) => {
+
+  const signer = newWallet ? newWallet : createRandomWallet()
   return new Masa({
     cookie: config.cookie || undefined,
-    wallet: newWallet,
+    wallet: signer,
     apiUrl: envs[env],
     environment: config.environment || 'dev',
     arweave: {
