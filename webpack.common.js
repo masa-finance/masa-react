9bbc9e46f49a92b67f311eedf168446b3c41ae44
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-02-03 14:40:57 +0100

    migrate to webpack, jest and eslint

---
+++ webpack.common.js
@@ -0,0 +1,87 @@
+'use strict';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { paths } = require('./webpack.parts.js');
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: paths.entry,
+  mode: 'none',
+  optimization: {
+    minimize: true,
+    emitOnErrors: false,
+  },
+  resolve: {
+    extensions: ['.js'],
+    modules: ['node_modules'],
+    fallback: {
+      os: false,
+      crypto: false,
+      url: false,
+      https: false,
+      http: false,
+      assert: false,
+      path: false,
+      stream: false,
+      zlib: false,
+      fs: false,
+    },
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        // Typescript loader
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        // CSS Loader
+        test: /\.css$/,
+        use: [
+          {
+            loader: PRODUCTION ? MiniCssExtractPlugin.loader : 'style-loader',
+          },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        // SCSS (SASS) Loader
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: PRODUCTION ? MiniCssExtractPlugin.loader : 'style-loader',
+          },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        // Less loader
+        test: /\.less$/,
+        use: [
+          {
+            loader: PRODUCTION ? MiniCssExtractPlugin.loader : 'style-loader',
+          },
+          { loader: 'css-loader' },
+          { loader: 'less-loader' },
+        ],
+      },
+      {
+        // Assets loader
+        // More information here https://webpack.js.org/guides/asset-modules/
+        test: /\.(gif|jpe?g|tiff|png|webp|bmp|svg|eot|otf|ttf|woff|woff2)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'assets/[name].[hash][ext][query]',
+        },
+      },
+    ],
+  },
+};
