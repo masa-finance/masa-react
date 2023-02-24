14c5ae122ce4be0f427d41dc5021d8938faa2b35
hide-on-bush-x <hideonbushx123@gmail.com>
2023-02-24 11:42:53 -0300

    Bundle config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { ProvidePlugin } = require('webpack');
@@ -10,7 +9,9 @@ module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
   return {
-    entry: './src/index.tsx',
+    entry: {
+      'main': './src/index.tsx',
+    },
     output: {
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       path: path.resolve(__dirname, 'dist'),
@@ -46,7 +47,7 @@ module.exports = (env, argv) => {
       ],
     },
     plugins: [
-      new CleanWebpackPlugin(),
+      // new CleanWebpackPlugin(),
       new ProvidePlugin({
         React: 'react',
       }),
