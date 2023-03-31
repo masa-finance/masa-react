4b5d3c82c543d2ad36a245b0d29225843d9f5bb6
aaron@masa.finance <aaron@masa.finance>
2023-03-30 19:29:59 -0700

    added use errors hook

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -6,6 +6,7 @@ export * from './masa-context-provider';
 export * from './masa-provider';
 export * from './masa-query-client';
 export * from './masa-shape';
+export * from './use-errors';
 export * from './use-debounce';
 export * from './use-local-storage';
 export * from './use-masa';
