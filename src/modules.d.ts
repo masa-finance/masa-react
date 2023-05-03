e1c54530faca2efc0c9a8579638eb42888ab1335
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-05-03 13:38:38 +0200

    fix modules

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,9 +1,7 @@
-import { MetaMaskInpageProvider } from '@metamask/providers';
-import { Ethereum } from '@wagmi/core';
+import './index';
 
 declare global {
   interface Window {
-    ethereum?: MetaMaskInpageProvider | Ethereum;
     RF?: {
       qualify: ({ code: string }) => void;
     };
