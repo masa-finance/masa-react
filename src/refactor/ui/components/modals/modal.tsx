9a1cd41e0c5bcf7ce4c309d2fd0d5b22ac5ee18b
simodrws <siamend@keyko.io>
2023-07-28 16:40:00 +0200

    feat: close modal on esc

--- src/refactor/ui/components/modals/modal.tsx
+++ src/refactor/ui/components/modals/modal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useCallback } from 'react';
+import React, { ReactNode, useCallback, useRef } from 'react';
 import Rodal from 'rodal';
 import { useModal } from '@ebay/nice-modal-react';
 
@@ -10,21 +10,22 @@ export const Modal = ({
   onClose?: () => unknown;
 }) => {
   const modal = useModal();
+  const modalRef = useRef(null);
   const handleClose = useCallback(async () => {
     onClose?.();
-    // await modal.hide();
-    console.log('closed modal');
+    await modal.hide();
   }, [modal, onClose]);
 
   return (
     <Rodal
+      closeOnEsc
       className="masa-rodal-container"
       visible={modal.visible}
       onClose={handleClose}
       width={550}
       height={615}
     >
-      <div className="masa-modal">
+      <div ref={modalRef} className="masa-modal">
         <div className="masa-modal-container">{children}</div>
       </div>
     </Rodal>
