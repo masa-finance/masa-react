8ad857efcc033ea0be7f64de9b7c4bd9f230b635
simodrws <siamend@keyko.io>
2023-08-11 16:19:10 +0200

    feat: more exports

--- src/refactor/masa-client/index.ts
+++ src/refactor/masa-client/index.ts
@@ -1,2 +1,4 @@
-export { useMasaClient } from './use-masa-client';
 export { useMasaSDK } from './use-masa-sdk';
+export { useMasaClient } from './use-masa-client';
+export * from './masa-query-client-context';
+export * from './use-masa-query-client';
