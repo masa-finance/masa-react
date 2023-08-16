4422289aad84edc0a829d47d4ba496cea80ea73a
simodrws <siamend@keyko.io>
2023-08-16 14:13:42 +0200

    chore: disable tests

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // moduleNameMapper: {
-  //   '^axios$': require.resolve('axios'),
-  //   '\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.ts',
-  // },
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+    '\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.ts',
+  },
 };
