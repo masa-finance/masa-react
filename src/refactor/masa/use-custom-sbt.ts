e997d15ab304346f1d96f3b2c9bfab8a099d0644
hide-on-bush-x <hideonbushx123@gmail.com>
2023-08-22 12:55:06 -0300

    gallery useCustomGallerySBT: Refactoring with new useQuery

--- src/refactor/masa/use-custom-sbt.ts
+++ src/refactor/masa/use-custom-sbt.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { Masa, MasaSBTWrapper } from '@masa-finance/masa-sdk';
 import { BigNumber } from 'ethers';
 import { useQuery } from '@tanstack/react-query';
@@ -7,67 +7,70 @@ import { MasaSBT } from '@masa-finance/masa-contracts-identity';
 import { useAsyncFn } from 'react-use';
 import { queryClient } from '../../provider';
 import { MasaQueryClientContext, useMasaClient } from '../masa-client';
-import { useWallet } from '../wallet-client';
 import { useConfig } from '../base-provider';
 import { CustomGallerySBT } from '../../components/masa-interface/pages/gallery/gallery';
 import { useCanQuery } from '../hooks/use-can-query';
 import { useSession } from './use-session';
 
-export const getCustomSBTsContractsQueryKey = ({
-  masa,
-  walletAddress,
-}: {
-  masa?: Masa;
-  walletAddress?: string; // unused
-}) => ['custom-sbt-contracts', walletAddress, masa?.config.networkName];
+export interface Token {
+  tokenId: BigNumber;
+  tokenUri: string;
+}
+export interface Metadata {
+  image: string;
+  name: string;
+  description: string;
+}
+export interface TokenWithMetadata extends Token {
+  metadata: Metadata;
+}
+export interface HydratatedContracts extends FullContract {
+  tokens: TokenWithMetadata[];
+}
 
-export const getCustomSBTsQueryKey = ({
-  masa,
-  walletAddress,
-}: {
-  masa?: Masa;
-  walletAddress?: string;
-}) => ['custom-sbt', walletAddress, masa?.config.networkName];
+export interface FullContract
+  extends CustomGallerySBT,
+    MasaSBTWrapper<MasaSBT> {}
 
-interface FullContract extends CustomGallerySBT, MasaSBTWrapper<MasaSBT> {}
 const fetchContracts = async (
   masa?: Masa,
   customGallerySBT?: CustomGallerySBT[]
-) => {
-  if (!masa) return [];
-  if (customGallerySBT && customGallerySBT.length > 0) {
-    const newContracts: FullContract[] = [];
-    for (const sbt of customGallerySBT) {
-      if (sbt.network && sbt.network !== masa.config.networkName) {
-        // eslint-disable-next-line no-continue
-        continue;
-      }
+): Promise<FullContract[]> => {
+  if (!masa || !customGallerySBT || customGallerySBT.length === 0) return [];
 
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const sbtContract = await masa?.sbt.connect(sbt.address);
+  const filteredSBTs = customGallerySBT.filter(
+    (sbt: CustomGallerySBT) =>
+      !sbt.network || sbt.network === masa.config.networkName
+  );
 
-        const contractObject = { ...sbtContract, ...sbt } as FullContract;
-
-        newContracts.push(contractObject);
-      } catch (error) {
-        console.log(error);
-      }
+  const contractPromises = filteredSBTs.map(async (sbt: CustomGallerySBT) => {
+    try {
+      const sbtContract = await masa.sbt.connect(sbt.address);
+      return { ...sbtContract, ...sbt } as FullContract;
+    } catch (error) {
+      console.log(error);
+      return null;
     }
-    return newContracts;
-  }
-  return [];
+  });
+
+  const contracts = await Promise.all(contractPromises);
+  return contracts.filter(Boolean) as FullContract[];
 };
 
 export const useCustomGallerySBT = () => {
-  const { sdk: masa } = useMasaClient();
-  const { address: walletAddress } = useWallet();
+  const { masaAddress, masaNetwork, sdk: masa } = useMasaClient();
+  const canQuery = useCanQuery();
+
   const { customSBTs } = useConfig();
 
-  const queryKey: (string | undefined)[] = useMemo(
-    () => getCustomSBTsContractsQueryKey({ masa, walletAddress }),
-    [walletAddress, masa, customSBTs]
-  );
+  const { hasSession, sessionAddress } = useSession();
+  const [, getContractsAsync] = useAsyncFn(async () => {
+    if (!canQuery) return null;
+    const result = await fetchContracts(masa, customSBTs);
+
+    if (!result) return null;
+    return result;
+  }, [canQuery, customSBTs, masa]);
 
   const {
     data: contracts,
@@ -76,14 +79,14 @@ export const useCustomGallerySBT = () => {
     isFetching,
     refetch: refetchContracts,
     error,
-  } = useQuery(queryKey, () => fetchContracts(masa, customSBTs), {
-    enabled: !!masa && !!walletAddress && !!customSBTs?.length,
-    retry: false,
-    onSuccess: (SBTs) => {
-      if (masa?.config.verbose) {
-        console.info('fetchCustomSBTs', SBTs);
-      }
-    },
+  } = useQuery({
+    queryKey: [
+      'custom-sbt-contracts',
+      { sessionAddress, masaAddress, masaNetwork, persist: false },
+    ],
+    enabled: !!hasSession && !!sessionAddress && !!masaAddress && !!masaNetwork,
+    context: MasaQueryClientContext,
+    queryFn: getContractsAsync,
   });
 
   return {
@@ -96,61 +99,50 @@ export const useCustomGallerySBT = () => {
   };
 };
 
-interface Token {
-  tokenId: BigNumber;
-  tokenUri: string;
-}
-interface Metadata {
-  image: string;
-  name: string;
-  description: string;
-}
-interface TokenWithMetadata extends Token {
-  metadata: Metadata;
-}
-interface HidratatedContracts extends FullContract {
-  tokens: TokenWithMetadata[];
-}
-
+// NEXT: refactor this function
 export const fetchCustomSBTs = async (
   customContracts: FullContract[]
-): Promise<HidratatedContracts[]> => {
-  const contracts = customContracts;
-  const hidratatedContracts: HidratatedContracts[] = []; // TBD
+): Promise<HydratatedContracts[]> => {
+  const hydratatedContractsPromises = customContracts.map(
+    async (contract: FullContract) => {
+      if (!contract.contract) return null;
 
-  for (const contract of contracts) {
-    // eslint-disable-next-line no-continue
-    if (!contract.contract) continue;
-    const hidratatedTokens: TokenWithMetadata[] = [];
+      try {
+        const tokens: Token[] = await contract.list();
 
-    try {
-      // eslint-disable-next-line no-await-in-loop
-      const tokens: Token[] = await contract.list();
+        // Fetch metadata for all tokens concurrently if the getMetadata method exists
+        const tokensWithMetadataPromises = contract.getMetadata
+          ? tokens.map(async (token: Token) => {
+              try {
+                const metadata = await contract.getMetadata(token);
+                return { metadata, ...token } as TokenWithMetadata;
+              } catch (error) {
+                console.log('METADATA ERROR', error);
+                return null;
+              }
+            })
+          : [];
 
-      if (contract.getMetadata) {
-        for (const token of tokens) {
-          try {
-            // eslint-disable-next-line no-await-in-loop
-            const metadata = await contract.getMetadata(token);
-            const tokenAndMetadata = { metadata, ...token };
-            hidratatedTokens.push(tokenAndMetadata);
-          } catch (error) {
-            console.log('METADTA ERROR', error);
-          }
-        }
+        let tokensWithMetadata = await Promise.all(tokensWithMetadataPromises);
+        tokensWithMetadata = tokensWithMetadata.filter(
+          Boolean
+        ) as TokenWithMetadata[];
+
+        return {
+          ...contract,
+          tokens: tokensWithMetadata,
+        } as HydratatedContracts;
+      } catch (error) {
+        console.log('LIST TOKENS ERROR', error);
+        return null;
       }
-    } catch (error) {
-      console.log('LIST TOKENS ERROR', error);
     }
+  );
 
-    const hidratatedContract = {
-      ...contract,
-      tokens: hidratatedTokens,
-    } as HidratatedContracts;
-    hidratatedContracts.push(hidratatedContract);
-  }
-
-  return hidratatedContracts;
+  const hydratatedContracts = (
+    await Promise.all(hydratatedContractsPromises)
+  ).filter(Boolean) as HydratatedContracts[];
+  return hydratatedContracts;
 };
 
 export const useCustomSBTsQuery = () => {
