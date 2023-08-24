af26d5ad40fb95270b7caed3b2349d779b4df856
hide-on-bush-x <hideonbushx123@gmail.com>
2023-08-24 12:17:16 -0300

    Improved Tab interface

--- src/refactor/ui/components/gallery/Tab.tsx
+++ src/refactor/ui/components/gallery/Tab.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 
-export const Tab = ({
-  title,
-  active,
-  onClick,
-}: {
+interface TabProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   active: boolean;
-  onClick: () => void;
-}) => (
+}
+
+export const Tab = ({ title, active, ...props }: TabProps) => (
   <button
     type="button"
     className={`tab ${active ? 'active' : ''}`}
-    onClick={onClick}
-    onKeyDown={(event) => {
-      if (event.key === 'Enter') {
-        onClick();
-      }
-    }}
+    {...props}
     key={`tab-${title}`}
   >
     {title}
