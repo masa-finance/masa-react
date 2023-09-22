e67c2234ec760192af57317a80c4e0e8ef10ed4f
simodrws <siamend@keyko.io>
2023-09-22 16:37:48 +0200

    feat: more exports

--- src/ui/components/modals/index.tsx
+++ src/ui/components/modals/index.tsx
@@ -2,3 +2,5 @@ export * from './authenticate';
 export * from './create-soulname';
 export * from './gallery';
 export * from './modal';
+export * from './ModalError';
+export * from './ModalLoading';
