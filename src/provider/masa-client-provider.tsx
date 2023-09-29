b8cb1673f4f77a47fbbcb34fd0ef719c903392b9
simodrws <siamend@keyko.io>
2023-09-29 09:37:00 +0200

    chore: remove commented code

--- src/provider/masa-client-provider.tsx
+++ src/provider/masa-client-provider.tsx
@@ -4,19 +4,19 @@ import { useMasaClient } from '../masa-client/use-masa-client';
 
 import { useIdentity } from '../masa/use-identity';
 import { useSession } from '../masa/use-session';
-import { useCreditScores } from '../masa/use-credit-scores';
-import { useSoulNames } from '../masa/use-soulnames';
-import { useGreen } from '../masa/use-green';
+// import type { useCreditScores } from '../masa/use-credit-scores';
+// import type { useSoulNames } from '../masa/use-soulnames';
+// import type { useGreen } from '../masa/use-green';
 import { useIdentityListen } from '../masa';
 import { useMasaQueryClient } from '../masa-client/use-masa-query-client';
 
 export interface MasaClientProviderValue {
-  masa?: ReturnType<typeof useMasaClient>['sdk'];
-  session?: ReturnType<typeof useSession>['session'];
-  identity?: ReturnType<typeof useIdentity>['identity'];
-  soulnames?: ReturnType<typeof useSoulNames>['soulnames'];
-  creditScores?: ReturnType<typeof useCreditScores>['creditScores'];
-  greens?: ReturnType<typeof useGreen>['greens'];
+  // masa?: ReturnType<typeof useMasaClient>['sdk'];
+  // session?: ReturnType<typeof useSession>['session'];
+  // identity?: ReturnType<typeof useIdentity>['identity'];
+  // soulnames?: ReturnType<typeof useSoulNames>['soulnames'];
+  // creditScores?: ReturnType<typeof useCreditScores>['creditScores'];
+  // greens?: ReturnType<typeof useGreen>['greens'];
 }
 
 export const MasaClientContext = createContext({} as MasaClientProviderValue);
@@ -36,9 +36,6 @@ export const MasaClientProvider = ({ children }: { children: ReactNode }) => {
     sessionAddress,
   } = useSession();
   const { identity, getIdentity } = useIdentity();
-  // const { creditScores } = useCreditScores();
-  // const { soulnames } = useSoulNames();
-  // const { greens } = useGreen();
 
   useIdentityListen({ identity, getIdentity, sessionAddress });
   // useSessionListen();
@@ -66,29 +63,8 @@ export const MasaClientProvider = ({ children }: { children: ReactNode }) => {
   ]);
 
   const masaClientProviderValue: MasaClientProviderValue = useMemo(
-    () =>
-      ({
-        // masa,
-        // session,
-        // sessionAddress,
-        // loginSession,
-        // logoutSession,
-        // identity,
-        // soulnames,
-        // creditScores,
-        // greens,
-      }) as MasaClientProviderValue,
-    [
-      // masa,
-      // identity,
-      // session,
-      // sessionAddress,
-      // loginSession,
-      // logoutSession,
-      // soulnames,
-      // creditScores,
-      // greens,
-    ]
+    () => ({}) as MasaClientProviderValue,
+    []
   );
   return (
     <MasaClientContext.Provider value={masaClientProviderValue}>
