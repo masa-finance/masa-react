416f4132403e067a4248d86b902e2358447329a0
simodrws <siamend@keyko.io>
2023-10-26 15:03:55 +0200

    fix: add loading session state to authenticate view

--- src/ui/components/modals/authenticate/use-authenticate-modal.ts
+++ src/ui/components/modals/authenticate/use-authenticate-modal.ts
@@ -16,7 +16,7 @@ export const useAuthenticateModal = ({
   onClose?: () => void;
 }) => {
   const { hasAddress, isConnected, signer, connector } = useWallet();
-  const { hasSession, loginSession } = useSession();
+  const { hasSession, loginSession, isLoadingSession } = useSession();
   const { company } = useConfig();
 
   const needsWalletConnection = useMemo(
@@ -24,8 +24,9 @@ export const useAuthenticateModal = ({
     [hasSession, isConnected, hasAddress]
   );
   const showAuthenticateView = useMemo(
-    () => isConnected && !hasSession && signer && hasAddress,
-    [isConnected, hasSession, signer, hasAddress]
+    () =>
+      isConnected && !hasSession && signer && hasAddress && !isLoadingSession,
+    [isConnected, hasSession, signer, hasAddress, isLoadingSession]
   );
 
   const successMessage = useMemo(() => {
