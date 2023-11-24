168e72bcec361057c9956c173209692bbde58fce
simodrws <siamend@keyko.io>
2023-11-24 14:53:16 +0100

    feat: add error handling to purchase hooks

--- src/masa/use-credit-scores-create.ts
+++ src/masa/use-credit-scores-create.ts
@@ -6,16 +6,28 @@ export const useCreditScoreCreate = () => {
   const { masa } = useMasaClient();
   const queryClient = useMasaQueryClient();
 
-  const [{ loading: isCreatingCreditScore }, createCreditScore] =
-    useAsyncFn(async () => {
+  const [
+    { loading: isCreatingCreditScore, error: errorCreditScoreCrate },
+    createCreditScore,
+  ] = useAsyncFn(async () => {
+    try {
       const response = await masa?.creditScore.create();
       await queryClient.invalidateQueries(['credit-scores']);
       return response?.success;
-    }, [masa, queryClient]);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        throw error;
+      }
+
+      throw new Error('Unknown error while creating a credit score');
+    }
+  }, [masa, queryClient]);
 
   return {
     createCreditScore,
     isCreatingCreditScore,
+    errorCreditScoreCrate,
     handleCreateCreditScore: createCreditScore,
   };
 };
