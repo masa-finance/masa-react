d2b9a1b3f2cc3a57143a0370cfa31fd92dfa7417
simodrws <siamend@keyko.io>
2024-01-23 16:44:27 +0100

    fix: masa-lint-config update

--- src/masa-client/query-client.ts
+++ src/masa-client/query-client.ts
@@ -43,7 +43,7 @@ export const createQueryClient = () => {
   }
 
   if (persister)
-    persistQueryClient({
+    void persistQueryClient({
       queryClient,
       persister,
       dehydrateOptions: {
