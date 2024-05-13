1a045142928162c8d1d227f644c09cbd7a261088
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-05-13 16:56:02 +0200

    refactor: remove useless tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^axios$': require.resolve('axios'),
-    '\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.ts',
-  },
-};
