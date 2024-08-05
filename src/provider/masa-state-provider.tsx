c680fd03456ace355993b8efe033b32509122763
juam <juanmanso@gmail.com>
2024-08-05 11:34:40 +0200

    feat: move QueryClientProvider to new wagmi + rainbowkit provider

--- src/provider/masa-state-provider.tsx
+++ src/provider/masa-state-provider.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
-import { QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClientProvider } from '@tanstack/react-query';
 import MasaClientProvider from './masa-client-provider';
-import { queryClient } from '../masa-client/query-client';
+// import { queryClient } from '../masa-client/query-client';
 
+// Commented the QueryClientProvider because we still need to test
+// if the new implementation is working or not.
 export const MasaStateProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MasaClientProvider>{children}</MasaClientProvider>
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    <MasaClientProvider>{children}</MasaClientProvider>
+    // </QueryClientProvider>
   );
 };
