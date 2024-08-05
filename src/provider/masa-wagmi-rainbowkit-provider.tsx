c6ddea5f67237d8ddf9ddd9d07fbee8a22476613
juam <juanmanso@gmail.com>
2024-08-05 12:18:13 +0200

    feat: add wagmi and rainbowkit providers using some sample chains

--- src/provider/masa-wagmi-rainbowkit-provider.tsx
+++ src/provider/masa-wagmi-rainbowkit-provider.tsx
@@ -1,11 +1,64 @@
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { PropsWithChildren } from 'react';
+import { Config, WagmiProvider } from 'wagmi';
+import * as allChains from 'wagmi/chains';
 
 // TODO: Consider importing the client from `masa-client/query-client`
 const client = new QueryClient();
 
+// Init with some sample chains
+const {
+  mainnet: ethereum,
+  goerli,
+  polygon,
+  bsc,
+  bscTestnet,
+  opBNB,
+  opBNBTestnet,
+  polygonAmoy,
+  polygonMumbai,
+  celo,
+  celoAlfajores,
+  base,
+  baseGoerli,
+  baseSepolia,
+  scroll,
+  scrollSepolia,
+} = allChains;
+
+const config: Config = getDefaultConfig({
+  appName: 'Masa App',
+  projectId: 'masa-app',
+  chains: [
+    ethereum,
+    goerli,
+    polygon,
+    bsc,
+    bscTestnet,
+    opBNB,
+    opBNBTestnet,
+    polygonAmoy,
+    polygonMumbai,
+    celo,
+    celoAlfajores,
+    base,
+    baseGoerli,
+    baseSepolia,
+    scroll,
+    scrollSepolia,
+  ],
+  ssr: true,
+});
+
 const MasaWagmiRainbowkitProvider = ({ children }: PropsWithChildren) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 };
 
 export default MasaWagmiRainbowkitProvider;
